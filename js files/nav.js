0cc712ceff3e04f5dd05f85c687d11d290a0ea9c
Erin Harper <erinharper24@gmail.com>
2023-09-11 02:23:43 +0200

    menu nav bar updated

--- js files/nav.js
+++ js files/nav.js
@@ -1,19 +1,27 @@
 var navlinks = document.getElementById("navlinks");
 
+var closeIcon; 
+var openIcon;
+
 function showMenu() {
   navlinks.style.left = "0";
+  openIcon.style.display = "none";
+  closeIcon.style.display = "block"; 
 }
 
 function hideMenu() {
   navlinks.style.left = "-200px";
+  openIcon.style.display = "block";
+  closeIcon.style.display = "none";
 }
 
 document.addEventListener("DOMContentLoaded", function() {
-  var closeIcon = document.querySelector('.fa-xmark');
-  var openIcon = document.querySelector('.fa-bars');
+  closeIcon = document.querySelector('.fa-solid.fa-xmark');
+  openIcon = document.querySelector('.fa-solid.fa-bars');
 
   if(closeIcon) {
     closeIcon.addEventListener('click', hideMenu);
+    closeIcon.style.display = "none";  // Ensure "xmark" is hidden on page load
   }
 
   if(openIcon) {
